7014f53611eb3aaff50f512b9e18d36002537b1b
Caleb Fernandes <calebfernandes3@gmail.com>
2023-09-16 01:33:35 -0400

    found a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Auth from "./pages/Auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Preferences from "./pages/Preferences";
+import Map from "./pages/Map";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Auth />} />
+          <Route path="/" element={<Auth />} />
+          <Route path="/preferences" element={<Preferences />} />
+          <Route path="/map" element={<Map />} />
         </Routes>
       </BrowserRouter>
     </div>
