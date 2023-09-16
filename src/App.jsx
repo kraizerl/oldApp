4ff9667e6047a5789d0b2663dac24bc493374c85
Michael Strickland <mpstrickland532@gmail.com>
2023-09-16 06:19:35 -0400

    added clients location

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import Auth from "./pages/Auth";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState, createContext, useEffect } from "react";
 import Preferences from "./pages/Preferences";
 import Map from "./pages/Map";
 
+export const GlobalVariables = createContext();
+
 function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  useEffect(()=>{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.log("Geolocation not supported");
+    }
+    
+    function success(position) {
+      setLatitude(() => position.coords.latitude);
+      setLongitude(() => position.coords.longitude);
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    }
+    
+    function error() {
+      console.log("Unable to retrieve your location");
+    }
+  }, 5000)
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
+      <GlobalVariables.Provider value={{longitude, setLongitude, latitude, setLatitude}}>
+      <Routes>
           <Route path="/" element={<Auth />} className="h-full" />
           <Route path="/preferences" element={<Preferences />} />
           <Route path="/map" element={<Map />} />
@@ -15,6 +40,7 @@ function App() {
         <Link to="/map" className="p-3 bg-black text-offwhite rounded-lg">
           MAP BUTTON
         </Link>
+      </GlobalVariables.Provider>
       </BrowserRouter>
     </div>
   );
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -3,13 +3,12 @@
 import React from "react";
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { GlobalVariables } from "../App";
 
 export default function Home() {
-  
-
   const {isLoaded} = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: "AIzaSyB0O1XNs5m_YOaBI-yniNxOTjL1NKGk0zg",
   });
   
   if (!isLoaded) return <div>Loading...</div>;
@@ -19,8 +18,10 @@ export default function Home() {
 
 function Map() {
   const [activeMarker, setActiveMarker] = useState(false);
+  const { longitude, setLongitude, latitude, setLatitude} = useContext(GlobalVariables);
+
   useEffect(() => {
     setActiveMarker((prev) => true)
     })
-  return <GoogleMap zoom={10} center={{lat: 44, lng: -80}} ><div className="h-screen">{activeMarker && (<Marker position={{lat:44, lng: -80}}/>)}</div></GoogleMap>
+  return <GoogleMap zoom={10} center={{lat: latitude, lng: longitude}} ><div className="h-screen">{activeMarker && (<Marker position={{lat:latitude, lng: longitude}}/>)}</div></GoogleMap>
 }
